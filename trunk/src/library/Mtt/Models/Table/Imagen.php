2575f47bd7e12a613264e813451608a207e57e95
Luis Alberto Mayta mamani <slovacus@gmail.com>
2011-09-26 17:45:47 -0500

    eliminar archivos

--- trunk/src/library/Mtt/Models/Table/Imagen.php
+++ trunk/src/library/Mtt/Models/Table/Imagen.php
@@ -5,13 +5,13 @@
  * and open the template in the editor.
  */
 
-/**
- * Description of Categoria
- *
- */
-class Mtt_Models_Table_Imagen extends Zend_Db_Table_Abstract
+
+class Mtt_Models_Table_Imagen
+        extends Mtt_Db_Table_Abstract
     {
 
     protected $_name = 'imagen';
+    protected $_primary = 'id';
+
 
     }
