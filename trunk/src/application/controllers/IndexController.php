443ad85558d349f0ed57002f0777c9bf7ee6fc22
Luis Alberto Mayta mamani <slovacus@gmail.com>
2011-09-26 10:11:26 -0500

    add new data

--- trunk/src/application/controllers/IndexController.php
+++ trunk/src/application/controllers/IndexController.php
@@ -17,23 +17,23 @@ class IndexController
 
     public function indexAction()
         {
-        $this->view->jQuery()
-                ->addJavascriptFile(
-                        'http://cdn.jquerytools.org/1.2.4/jquery.tools.min.js'
-                )
-                ->addStylesheet(
-                        'http://flowplayer.org/tools/css/scrollale-buttons.css'
-                )
-//                ->addOnLoad(
-//                        "alert('hola kusanagui');"
+//        $this->view->jQuery()
+//                ->addJavascriptFile(
+//                        'http://cdn.jquerytools.org/1.2.4/jquery.tools.min.js'
 //                )
-        ;
+//                ->addStylesheet(
+//                        'http://flowplayer.org/tools/css/scrollale-buttons.css'
+//                )
+////                ->addOnLoad(
+////                        "alert('hola kusanagui');"
+////                )
+//        ;
         $this->view->assign(
                 'oferEquipo' , $this->_equipo->showEquiposOfers()
         );
 
-        $paginado = new Zend_Paginator_Adapter_Array( range( 1 , 9 ) );
-        $this->view->assign( 'items' , $paginado );
+//        $paginado = new Zend_Paginator_Adapter_Array( range( 1 , 9 ) );
+//        $this->view->assign( 'items' , $paginado );
         }
 
 
