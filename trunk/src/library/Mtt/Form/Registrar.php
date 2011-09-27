f4b8799daece77b3be614da0f3b129e9ff2354b1
Luis Alberto Mayta mamani <slovacus@gmail.com>
2011-09-27 09:54:40 -0500

    agregando validacion de Email

--- trunk/src/library/Mtt/Form/Registrar.php
+++ trunk/src/library/Mtt/Form/Registrar.php
@@ -44,8 +44,13 @@ class Mtt_Form_Registrar
         $this->addElement( $e );
 
         $e = new Zend_Form_Element_Text( 'email' );
+        $e->addValidator( new Zend_Validate_Db_RecordExists(
+                        array( 'table' => 'usuario' ,
+                            'field' => 'email' )
+                )
+        );
         $e->setRequired();
-        //$e->setDecorators( array( $decorator ) );
+
         $e->setLabel( 'Email:' );
         $e->addValidator( new Zend_Validate_EmailAddress() );
         $this->addElement( $e );
