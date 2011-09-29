f2049155924aaa9e23e467025fe814240b273421
Luis Alberto Mayta mamani <slovacus@gmail.com>
2011-09-29 16:39:21 -0500

    cambios

---
+++ trunk/src/library/Mtt/Models/Table/Producto.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+class Mtt_Models_Table_Producto
+        extends Mtt_Db_Table_Abstract
+    {
+
+    protected $_name = 'equipo';
+    protected $_primary = 'id';
+
+
+    }
+
