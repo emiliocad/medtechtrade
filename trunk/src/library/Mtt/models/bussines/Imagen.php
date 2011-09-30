d23f21aece6acc754d98891a62e0ec593cc00584
Luis Alberto Mayta mamani <slovacus@gmail.com>
2011-09-30 10:34:19 -0500

    agregando datos

--- trunk/src/library/Mtt/models/bussines/Imagen.php
+++ trunk/src/library/Mtt/models/bussines/Imagen.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 <?php
 
@@ -69,74 +68,3 @@ class Mtt_Models_Bussines_Imagen
         }        
 
     }
-=======
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-
-class Mtt_Models_Bussines_Imagen
-        extends Mtt_Models_Table_Imagen
-    {
-    
-    public function getFindId( $id )
-        {
-
-        return $this->fetchRow( 'id = ' . $id );
-        }
-
-    public function listByEquip( $idEquipo )
-        {
-        $db = $this->getAdapter();
-        $query = $db->select()
-                ->from( $this->_name )
-                ->where( 'active = ?' , self::ACTIVE )
-                ->where( 'equipo_id = ?' , $idEquipo )
-                ->query()
-        ;
-
-        return $query->fetchAll( Zend_Db::FETCH_OBJ );
-        }
-
-        
-
-    public function updateImagen( array $data , $id )
-        {
-
-        $this->update( $data , 'id = ' . $id );
-        }
-
-
-    public function saveImagen( array $data )
-        {
-
-        $this->insert( $data );
-        }
-
-
-    public function deleteImagen( $id )
-        {
-
-        $this->delete( 'id = ?' , $id );
-        }
-
-
-    public function activarImagen( $id )
-        {
-
-        $this->update( array( "active" => self::ACTIVE ) , 'id = ' . $id );
-        }
-
-
-    public function desactivarImagen( $id )
-        {
-
-        $this->update( array( "active" => self::DESACTIVATE ) , 'id = ' . $id );
-        }        
-
-    }
->>>>>>> 39e26dc619c0e0384a65b352a41de6ebfb53bd11
--- trunk/src/library/Mtt/models/table/Producto.php
+++ trunk/src/library/Mtt/models/table/Producto.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 <?php
 
@@ -17,23 +16,3 @@ class Mtt_Models_Table_Producto
 
 
     }
-=======
-<?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-class Mtt_Models_Table_Producto
-        extends Mtt_Db_Table_Abstract
-    {
-
-    protected $_name = 'equipo';
-    protected $_primary = 'id';
-
-
-    }
-
->>>>>>> 39e26dc619c0e0384a65b352a41de6ebfb53bd11
