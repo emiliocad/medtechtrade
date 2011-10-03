92a411c910211e943299b9ac2d48008466bd5da2
teresa Chunga <tj.chunga@gmail.com>
2011-10-03 10:12:28 -0500

    arreglando interfaz detalle de usuario

--- trunk/src/application/modules/admin/controllers/UserController.php
+++ trunk/src/application/modules/admin/controllers/UserController.php
@@ -17,6 +17,13 @@ class Admin_UserController
 
     public function indexAction()
         {
+        $this->view->jQuery()
+                ->addOnLoad(
+                        ' $(document).ready(function() {
+                            $("#tabs").tabs();
+                          });'
+                )
+        ;
         $this->view->assign(
                 'usuarios' , $this->_user->listar()
         );
@@ -30,13 +37,7 @@ class Admin_UserController
         $this->_helper->layout->disableLayout();
         
         $id = intval( $this->_getParam( 'id' ) );
-        $this->view->jQuery()
-                ->addOnLoad(
-                        ' $(document).ready(function() {
-                            $("#tabs").tabs();
-                          });'
-                )
-        ;
+        
         //Editar datos del usuario
         $this->editarAction();
         
