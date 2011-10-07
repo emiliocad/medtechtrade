7ecbaf518db9678bc85345a5f3f9478ceb633040
Luis Alberto Mayta mamani <slovacus@gmail.com>
2011-10-06 20:14:24 -0500

    agregando finance

---
+++ trunk/src/library/Mtt/Service/Google/Finance.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class Mtt_Service_Google_Finance
+    {
+
+    private $moneda;
+    private $country;
+    
+
+    public function __construct()
+        {
+        
+        }
+
+
+    public static function convert()
+        {
+        
+        }
+
+
+    }
