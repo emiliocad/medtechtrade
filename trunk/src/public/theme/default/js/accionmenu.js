46196d3bf1128399a6253930d5d00233fad4c7bb
tjche <tj.chunga@gmail.com>
2011-10-07 08:35:50 -0500

    modificacion de layout para menu-visitante

--- trunk/src/public/theme/default/js/accionmenu.js
+++ trunk/src/public/theme/default/js/accionmenu.js
@@ -1,22 +1,22 @@
 
 $(document).ready(function(){
-     var id = readCookie('tabVisible');
-     $("#sections-buttons-top #"+id).addClass("selected");
-   // $("#sections-buttons-top ul li" ).click(function(e){
-     //   var a = e.target.id;  
-      //  alert(a);
-    //    $("#sections-buttons-top ul li.selected" ).removeClass("selected");
-    //    $("#sections-buttons-top #"+a).addClass("selected"); 
-        //$(this).toggleClass('selected'); 
-        //    createCookie('tabVisible',1,1)
-    //});
+    var id = readCookie('tabVisible');
+    $("#sections-buttons-top #"+id).addClass("selected");
+// $("#sections-buttons-top ul li" ).click(function(e){
+//   var a = e.target.id;  
+//  alert(a);
+//    $("#sections-buttons-top ul li.selected" ).removeClass("selected");
+//    $("#sections-buttons-top #"+a).addClass("selected"); 
+//$(this).toggleClass('selected'); 
+//    createCookie('tabVisible',1,1)
+//});
     
     
     
 });
 
-function funcion1(id){
-    alert(id);
+function fnSelectedItem(id)
+    {
     createCookie('tabVisible',id,1);
    
-}
+    }
