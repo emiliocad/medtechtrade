8778f554d05d84408a62e07b305a0c354de5dc62
tjche <tj.chunga@gmail.com>
2011-10-07 00:38:19 -0500

    opcion de menu seleccionado

--- trunk/src/public/theme/default/js/accionmenu.js
+++ trunk/src/public/theme/default/js/accionmenu.js
@@ -1,10 +1,22 @@
 
 $(document).ready(function(){
-    
-    //$("#sections-buttons-top ul li" ).click(function(e){
-    //    var a = e.target.id;  
+     var id = readCookie('tabVisible');
+     $("#sections-buttons-top #"+id).addClass("selected");
+   // $("#sections-buttons-top ul li" ).click(function(e){
+     //   var a = e.target.id;  
+      //  alert(a);
     //    $("#sections-buttons-top ul li.selected" ).removeClass("selected");
     //    $("#sections-buttons-top #"+a).addClass("selected"); 
         //$(this).toggleClass('selected'); 
-   // });
+        //    createCookie('tabVisible',1,1)
+    //});
+    
+    
+    
 });
+
+function funcion1(id){
+    alert(id);
+    createCookie('tabVisible',id,1);
+   
+}
