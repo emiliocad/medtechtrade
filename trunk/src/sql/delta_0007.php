3d0407c63e4c61ef4397be3fbdbccb42cfd1ad8d
Luis Alberto Mayta mamani <slovacus@gmail.com>
2011-10-07 17:20:56 -0500

    agregando delta

---
+++ trunk/src/sql/delta_0007.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class Delta_0007
+        extends Mtt_Migration_Delta
+    {
+
+    protected $_author = "Luis Alberto Mayta Mamani";
+    protected $_desc = "Agregando CAmbios A la 
+        Tabla Moneda para Google Finance";
+
+
+    public function up()
+        {
+        $sql =
+                "ALTER TABLE moneda
+                    ADD cambio NUMERIC(8,3)";
+
+        $this->_db->query( $sql );
+        $sql =
+                "ALTER TABLE moneda
+                        ADD fechacambio DATE";
+
+        $this->_db->query( $sql );
+        return true;
+        }
+
+
+    }
