2efe9b8d8561be13fddc45414abab1fec534d021
luis alberto mayta mamani <slovacus@gmail.com>
2011-10-08 07:05:31 -0500

    agregando cambios

--- trunk/src/library/Mtt/models/bussines/Categoria.php
+++ trunk/src/library/Mtt/models/bussines/Categoria.php
@@ -27,11 +27,7 @@ class Mtt_Models_Bussines_Categoria
         $db = $this->getAdapter();
 
         $query = $db->select()
-<<<<<<< HEAD
-                ->from( 'equipo' , array( 'id' , 'nombre' ) )
-=======
                 ->from( 'equipo' , array( 'id' , 'nombre' , 'modelo' ) )
->>>>>>> developer
                 ->joinInner( $this->_name ,
                              'categoria.id = equipo.categoria_id ' ,
                              array( 'categoria.nombre as categoria' )
@@ -45,8 +41,6 @@ class Mtt_Models_Bussines_Categoria
                 )
                 ->where( 'equipo.active IN (?)' , self::ACTIVE )
                 ->where( 'equipo.categoria_id IN (?)' , $id )
-<<<<<<< HEAD
-=======
                 ->order( $order )
                 ->query();
 
@@ -79,7 +73,6 @@ class Mtt_Models_Bussines_Categoria
                 ->where( 'equipo.active IN (?)' , self::ACTIVE )
                 ->where( 'equipo.categoria_id IN (?)' , $data->id )
                 ->order( $order )
->>>>>>> developer
                 ->query();
 
         return $query->fetchAll( Zend_Db::FETCH_OBJ );
@@ -101,123 +94,91 @@ class Mtt_Models_Bussines_Categoria
     public function listCategory()
         {
 
-<<<<<<< HEAD
-        
         $db = $this->getAdapter();
 
         $query = $db->select()
-                ->from( $this->_name)
-                ->where( 'active IN (?)' , self::ACTIVE )
- 
-                ->query();
-
-        return $query->fetchAll( Zend_Db::FETCH_OBJ );
-        
-        }
-
-
-       
-
-
-=======
-        $db = $this->getAdapter();
-
-        $query = $db->select()
-                ->from( $this->_name )
-                ->where( 'active IN (?)' , self::ACTIVE )
-                ->query();
+        ->from( $this->_name )
+        ->where( 'active IN (?)' , self::ACTIVE )
+        ->query();
 
         return $query->fetchAll( Zend_Db::FETCH_OBJ );
         }
 
 
->>>>>>> developer
-    public function getCategoria( $id )
-        {
-        $db = $this->getAdapter();
-        $query = $db->select()
-                ->from( $this->_name )
-                ->where( 'id IN (?)' , $id )
-                ->where( 'active = ?' , '1' )
-                ->query()
-        ;
-        return $query->fetchObject();
-        }
+
+public function getCategoria( $id )
+{
+$db = $this->getAdapter();
+$query = $db->select()
+->from( $this->_name )
+->where( 'id IN (?)' , $id )
+->where( 'active = ?' , '1' )
+->query()
+;
+return $query->fetchObject();
+}
 
 
-<<<<<<< HEAD
-=======
-    public function getCategoriaBySlug( $slug )
-        {
-        $db = $this->getAdapter();
-        $query = $db->select()
-                ->from( $this->_name )
-                ->where( 'slug IN (?)' , $slug )
-                ->where( 'active = ?' , self::ACTIVE )
-                ->query()
-        ;
-        return $query->fetchObject();
-        }
+public function getCategoriaBySlug( $slug )
+{
+$db = $this->getAdapter();
+$query = $db->select()
+        ->from( $this->_name )
+        ->where( 'slug IN (?)' , $slug )
+        ->where( 'active = ?' , self::ACTIVE )
+        ->query()
+;
+return $query->fetchObject();
+}
 
 
->>>>>>> developer
-    public function updateCategoria( array $data , $id )
-        {
+public function updateCategoria( array $data , $id )
+{
 
-        $this->update( $data , 'id = ' . $id );
-        }
-
-
-    public function saveCategoria( array $data )
-        {
-<<<<<<< HEAD
-
-=======
-        $slug = new Mtt_Filter_Slug( array(
-                    'field' => 'slug' ,
-                    'model' => $this
-                        ) );
-
-        $dataNew = array(
-            'slug' => $slug->filter( $data['title'] )
-        );
-
-        $data = array_merge( $dataNew , $data );
->>>>>>> developer
-        $this->insert( $data );
-        }
+$this->update( $data , 'id = ' . $id );
+}
 
 
-    public function deleteCategoria( $id )
-        {
+public function saveCategoria( array $data )
+{
 
-        $this->delete( 'id = ?' , $id );
-        }
+$slug = new Mtt_Filter_Slug( array(
+            'field' => 'slug' ,
+            'model' => $this
+                ) );
 
+$dataNew = array(
+    'slug' => $slug->filter( $data['title'] )
+);
 
-    public function activarCategoria( $id )
-        {
-
-        $this->update( array(
-            "active" => self::ACTIVE )
-                , 'id = ' . $id );
-        }
-
-
-    public function desactivarCategoria( $id )
-        {
-
-        $this->update( array(
-            "active" => self::DESACTIVATE )
-                , 'id = ' . $id );
-        }
+$data = array_merge( $dataNew , $data );
+$this->insert( $data );
+}
 
 
-<<<<<<< HEAD
+public function deleteCategoria( $id )
+{
 
-   
+$this->delete( 'id = ?' , $id );
+}
 
-=======
 
->>>>>>> developer
-    }
+public function activarCategoria( $id )
+{
+
+$this->update( array(
+    "active" => self::ACTIVE )
+        , 'id = ' . $id );
+}
+
+
+public function desactivarCategoria( $id )
+{
+
+$this->update( array(
+    "active" => self::DESACTIVATE )
+        , 'id = ' . $id );
+}
+
+
+}
