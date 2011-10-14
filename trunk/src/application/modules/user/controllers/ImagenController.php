ad267fa6a3a158c167ae5887f501481dc227e57f
Luis Alberto Mayta mamani <slovacus@gmail.com>
2011-10-14 12:27:52 -0500

    agregando cambios template de pdf

--- trunk/src/application/modules/user/controllers/ImagenController.php
+++ trunk/src/application/modules/user/controllers/ImagenController.php
@@ -1,9 +1,12 @@
 <?php
 
-class User_ImagenController extends Mtt_Controller_Action
+
+class User_ImagenController
+        extends Mtt_Controller_Action
     {
 
     protected $_imagen;
+
 
     public function init()
         {
@@ -11,23 +14,25 @@ class User_ImagenController extends Mtt_Controller_Action
         $this->_imagen = new Mtt_Models_Bussines_Imagen();
         }
 
+
     public function indexAction()
         {
-       
+        
         }
+
 
     public function editarAction()
-        {         
+        {
         
         }
 
 
-    public function nuevoAction( )
+    public function nuevoAction()
         {
-        $idEquipo = ( int ) ( $this->_getParam( 'id' , null ) );
-        
+        $idEquipo = ( int ) $this->_getParam( 'id' , null );
+
         $form = new Mtt_Form_Imagen();
-        
+
         if ( $this->_request->isPost()
                 &&
                 $form->isValid( $this->_request->getPost() )
@@ -37,41 +42,44 @@ class User_ImagenController extends Mtt_Controller_Action
             unset( $data["MAX_FILE_SIZE"] );
             unset( $data["submit"] );
 
-            $imagen = $form->getValues();            
+            $imagen = $form->getValues();
             $upload = $form->imagen->getTransferAdapter();
-            
+
             $target = str_replace(
-                        ' ' , '' , $nombre
-                ) . '_' . $id . '.jpg';
+                            ' ' , '' , $nombre
+                    ) . '_' . $idEquipo . '.jpg';
 
             $f = new Zend_Filter_File_Rename(
-                        array(
-                            'target' => $imagen->nombre ,
-                            'overwrite' => true
-                        )
+                            array(
+                                'target' => $imagen->nombre ,
+                                'overwrite' => true
+                            )
             );
 
-            $upload->addFilter( $f );             
+            $upload->addFilter( $f );
             $imagen_new = array(
                 'equipo_id' => $idEquipo
             );
-        
+
             if ( $form->imagen->receive() )
-                    {
+                {
 
-                    $arrayTmp = array( 'imagen' => $target );
+                $arrayTmp = array( 'imagen' => $target );
 
-                    $imagen = array_merge( $imagen , $imagen_new );
+                $imagen = array_merge( $imagen , $imagen_new );
 
-                    $this->_imagen->saveImagen( $imagen , $id );
-                    $this->_helper->FlashMessenger( 'Se inserto nueva imagen' );
-                    $this->_redirect( 'user/equipo/ver/id'.$idEquipo );
-                    }
-        
-            
+                $this->_imagen->saveImagen( $imagen , $id );
+                $this->_helper->FlashMessenger(
+                        $this->_translate->translate(
+                                'Se inserto nueva imagen'
+                        )
+                );
+                $this->_redirect( 'user/equipo/ver/id/' . $idEquipo );
+                }
             }
-            $this->view->assign( 'frmRegistrar' , $form );
+        $this->view->assign( 'frmRegistrar' , $form );
         }
+
 
     }
 
