fffa08d3992666395fc8916472426a87baf45084
Luis Alberto Mayta mamani <slovacus@gmail.com>
2011-10-17 19:00:53 -0500

    add checkout 2.0

--- trunk/src/application/modules/user/controllers/CheckoutController.php
+++ trunk/src/application/modules/user/controllers/CheckoutController.php
@@ -5,25 +5,27 @@ class User_CheckoutController
         extends Mtt_Controller_Action
     {
 
+    protected $mtt;
+
 
     public function init()
         {
         parent::init();
-        /* Initialize action controller here */
+        $this->mtt = new Zend_Session_Namespace( 'MTT' );
         }
 
 
     public function indexAction()
         {
         $id = ( int ) $this->getRequest()->getParam( 'id' );
-        
-        $equipo = new Mtt_Models_Bussines_Equipo();
-        //$equipo->getFindId( $id )
-        //$item = new Mtt_Store_Cart_Item( $equipo );
 
+        $equipo = new Mtt_Models_Bussines_Equipo();
+        $this->mtt->cart = $equipo->getFindId( $id );
+        //
+        //$item = new Mtt_Store_Cart_Item( $equipo );
         //$this->view->assign('carro' , $value);
-        
-        $this->view->assign( 'equipo' , $id );
+
+        $this->view->assign( 'equipo' , $this->mtt->cart );
         }
 
 
