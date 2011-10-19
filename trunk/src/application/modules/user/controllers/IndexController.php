a9ffbe464cadc8f284306e48bc3dc22fc7ccf909
Luis Alberto Mayta mamani <slovacus@gmail.com>
2011-10-19 18:01:31 -0500

    add nuevos equipos

--- trunk/src/application/modules/user/controllers/IndexController.php
+++ trunk/src/application/modules/user/controllers/IndexController.php
@@ -5,10 +5,18 @@ class User_IndexController
         extends Mtt_Controller_Action
     {
 
+    protected $_usuario;
+
 
     public function indexAction()
         {
-       
+        $this->_usuario = new Mtt_Models_Bussines_Equipo();
+        $this->view->assign(
+                'lastequipos' ,
+                $this->_usuario->getLastEquipmentPublished(
+                        $this->authData['usuario']->id
+                )
+        );
         }
 
 
