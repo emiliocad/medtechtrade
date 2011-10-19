5bcaaf52a8167c8823ba74e6f314ba5c91360ab3
Luis Alberto Mayta mamani <slovacus@gmail.com>
2011-10-19 18:17:58 -0500

    add nuevos cambios

--- trunk/src/application/modules/user/controllers/IndexController.php
+++ trunk/src/application/modules/user/controllers/IndexController.php
@@ -5,16 +5,27 @@ class User_IndexController
         extends Mtt_Controller_Action
     {
 
-    protected $_usuario;
+    protected $_equipo;
+    protected $_reserva;
 
 
     public function indexAction()
         {
-        $this->_usuario = new Mtt_Models_Bussines_Equipo();
+        $this->_reserva = new Mtt_Models_Bussines_Reserva();
+        $this->_equipo = new Mtt_Models_Bussines_Equipo();
+
         $this->view->assign(
                 'lastequipos' ,
-                $this->_usuario->getLastEquipmentPublished(
+                $this->_equipo->getLastEquipmentPublished(
                         $this->authData['usuario']->id
+                )
+        );
+
+        $this->view->assign(
+                'favoritos' ,
+                $this->_reserva->getReservaByUser(
+                        $this->authData['usuario']->id ,
+                        Mtt_Models_Table_TipoReserva::FAVORITE
                 )
         );
         }
