66c7cd26d7d899b3c1542ab76ec92aafba1e6f7b
tjche <tj.chunga@gmail.com>
2011-10-21 17:26:57 -0500

    commit modelo fabricante

--- trunk/src/library/Mtt/Models/Bussines/Fabricante.php
+++ trunk/src/library/Mtt/Models/Bussines/Fabricante.php
@@ -5,23 +5,17 @@
  * and open the template in the editor.
  */
 
+class Mtt_Models_Bussines_Fabricante 
+    extends Mtt_Models_Table_Fabricante {
 
-class Mtt_Models_Bussines_Fabricante
-        extends Mtt_Models_Table_Fabricante
-    {
-
-
-    public function getComboValues()
-        {
-        $filas = $this->fetchAll( 'active=1' )->toArray();
-        $values = array( );
-        foreach ( $filas as $fila )
-            {
+    public function getComboValues() {
+        $filas = $this->fetchAll('active=1')->toArray();
+        $values = array();
+        foreach ($filas as $fila) {
             $values[$fila['id']] = $fila['nombre'];
-            }
-        return $values;
         }
-
+        return $values;
+    }
 
 //    public function getFindId( $id )
 //        {
@@ -35,52 +29,57 @@ class Mtt_Models_Bussines_Fabricante
 //        return $this->fetchRow( 'id = ' . $id );
 //        }
 
-    public function listar()
-        {
+    public function listar($active ) {
         $db = $this->getAdapter();
         $query = $db->select()
-                ->from( $this->_name )
-                ->where( 'active = ?' , '1' )
+                ->from($this->_name)
+                ->where( 'active in (?)' , $active )
                 ->query()
         ;
 
-        return $query->fetchAll( Zend_Db::FETCH_OBJ );
-        }
-
-
-    public function updateFabricante( array $data , $id )
-        {
-
-        $this->update( $data , 'id = ' . $id );
-        }
-
-
-    public function saveFabricante( array $data )
-        {
-
-        $this->insert( $data );
-        }
-
-
-    public function deleteFabricante( $id )
-        {
-
-        $this->delete( 'id = ?' , $id );
-        }
-
-
-    public function activarFabricante( $id )
-        {
-
-        $this->update( array( "active" => self::ACTIVE ) , 'id = ' . $id );
-        }
-
-
-    public function desactivarFabricante( $id )
-        {
-
-        $this->update( array( "active" => self::DESACTIVATE ) , 'id = ' . $id );
-        }
-
-
+        return $query->fetchAll(Zend_Db::FETCH_OBJ);
     }
+
+    
+    public function pagListManufactur($active = null) {
+        $_conf = new Zend_Config_Ini(
+                        APPLICATION_PATH . '/configs/myConfigAdmin.ini'
+                , 'fabricante'
+        );
+        $data = $_conf->toArray();
+
+        $value = ($active == null) ? array(0,1) : 1;
+        
+        $object = Zend_Paginator::factory($this->listar($value));
+        $object->setItemCountPerPage(
+                $data['ItemCountPerPage']
+        );
+        return $object;
+    }
+
+    public function updateFabricante(array $data, $id) {
+
+        $this->update($data, 'id = ' . $id);
+    }
+
+    public function saveFabricante(array $data) {
+
+        $this->insert($data);
+    }
+
+    public function deleteFabricante($id) {
+
+        $this->delete('id =' . (int) $id);
+    }
+
+    public function activarFabricante($id) {
+
+        $this->update(array("active" => self::ACTIVE), 'id = ' . $id);
+    }
+
+    public function desactivarFabricante($id) {
+
+        $this->update(array("active" => self::DESACTIVATE), 'id = ' . $id);
+    }
+
+}
